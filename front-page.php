5990b2e16cf92395c5da504a751b98c772891430
usrkay <160225786+usrkay@users.noreply.github.com>
2024-02-16 15:33:44 +0545

    Add files via upload
    notor theme

---
+++ front-page.php
@@ -0,0 +1,279 @@
+<!-- Slider starts here -->
+
+
+<div class = 'uk-section uk-section-large uk-width-1-1@m uk-width-1-1@s uk-inline' id="slidernav">
+
+  <div uk-slideshow = "autoplay: true;animation:pull;finite: false;">
+
+  <ul uk-slideshow-item ="previous" class="uk-position-medium uk-position-center-left" style="font-size:90px;" id="slidernav">  <a href="#">    &lt</a></ul>
+  <ul uk-slideshow-item ="next"  class="uk-position-medium uk-position-center-right"  style="font-size:90px;" id="slidernav">  <a href="#">   &gt</a></ul>
+            <ul class="uk-slideshow-items">
+
+
+
+
+                  <li uk-slideshow-item="0">
+				  <img src='<?php bloginfo('template_directory')?>/uhdpics/wallpaper-1.png' uk-cover>
+                        <div class=" uk-position-center">
+
+                                <div class="uk-transition-slide-bottom ">
+                                            <h1  uk-text-large uk-text-bolder><a href="#">
+We Create Better Apps for your Organization.
+						</a></h1>
+
+                                            <h2 uk-text-large><a href="#">Try Our Apps</a></h2>
+                                            <button class="uk-button uk-button-default uk-button-large" ><a href="#">Contact Us</a></button>
+
+                                </div>
+
+                            </div>
+
+                  </li>
+
+
+                    <li uk-slideshow-item="1">
+					<img src='<?php bloginfo('template_directory')?>/uhdpics/deliveryapps.jpg' uk-cover>
+                        <div class="uk-position-center">
+
+                                <div class="uk-transition-slide-bottom">
+                                  <h1  uk-text-large uk-text-bolder><a href="#">
+					We Deliver To Your Doorstep</a>
+					</h1>
+
+                                  <h2 uk-text-large><a href="#">Try Our Apps</a></h2>
+                                  <button class="uk-button uk-button-default uk-button-large" ><a href="#">Contact Us</a></button>
+
+                                </div>
+
+                            </div>
+
+                    </li>
+
+
+                   <li uk-slideshow-item="2">
+				   <img src='<?php bloginfo('template_directory')?>/uhdpics/wallpaper-6.png' uk-cover>
+                        <div class="uk-position-center">
+
+                                <div class="uk-transition-slide-bottom">
+                                  <h1  uk-text-large uk-text-bolder><a href="#">
+					We Visit You On Your Demands				
+				</a></h1>
+
+                                  <h2 uk-text-large> <a href="#">Try Our Apps</a></h2>
+                                  <button class="uk-button uk-button-default uk-button-large" ><a href="#">Contact Us</a></button>
+
+                                </div>
+
+                            </div>
+                  </li>
+
+
+
+                          <li uk-slideshow-item="3">
+                             <img src='<?php bloginfo('template_directory')?>/uhdpics/wallpaper-0.png' uk-cover>
+                              <div class="uk-position-center">
+
+                                      <div class="uk-transition-slide-bottom">
+                                        <h1  uk-text-large uk-text-bolder><a href="#">We Search The Best For You</a></h1>
+
+                                        <h2 uk-text-large><a href="#">Try Our Apps</a></h2>
+                                        <button class="uk-button uk-button-default uk-button-large" ><a href="#">Contact Us</a></button>
+
+                                      </div>
+
+                                  </div>
+
+                          </li>
+
+
+                        <li uk-slideshow-item="4">
+                              <img src='<?php bloginfo('template_directory')?>/uhdpics/wallpaper-7.png' uk-cover>
+                              <div class="uk-position-center">
+
+                                      <div class="uk-transition-slide-bottom">
+                                        <h1  uk-text-large uk-text-bolder><a href="#">We Value Your Norms And Respect Your Opinion</a></h1>
+
+                                        <h2 uk-text-large><a href="#">Try Our Apps</a></h2>
+                                        <button class="uk-button uk-button-default uk-button-large" ><a href="#">Contact Us</a></button>
+
+                                      </div>
+
+                                  </div>
+
+                        </li>
+
+                      </ul>
+
+          <ul class="uk-position-bottom-center uk-position-medium uk-slideshow-nav uk-dotnav" id="slidernav"></ul>
+  </div>
+</div>
+<!-- Slider ends here -->
+
+
+
+
+<div class = 'uk-section uk-section-large  uk-padding  uk-grid ' id="aboutus">
+										 <div class='uk-container uk-container-small uk-width-1-1@s  uk-width-1-1@m'>
+																							
+																								<center><h1 uk-parallax="x:0,80,0"	>ABOUT OUR COMPANY</h1></center>
+																								
+										 </div>
+ 
+                          <div class='uk-container uk-container-small  uk-width-1-1@s  uk-width-1-2@m'>
+
+
+                                  <div class="uk-card uk-card-default">
+                                                                   
+                                                                    <div class="uk-card-body">
+                                                                          <p uk-parallax="x:0,80,0"  uk-text-small uk-text-bolder>	
+																					  •<b>W</b>e are a software development platform .<br><br>
+                                                                                      •<b>W</b>e offer our clients with deliberate services.<br><br>
+                                                                                      •<b>W</b>e provide all kinds of IT services to manage your workplace.<br><br>
+                                                                                      •<b>W</b>e  deliver to you the best and provide maintainance with no costs.<br><br>
+																					  •<b>W</b>e value your opinions and fulfill all your requirements to your doorstep.<br><br>
+																					  •<b>W</b>e are experienced with multiple development  platforms.<br><br>
+																					  
+                                                                          </p>
+																		  																	
+																						<button class="uk-button uk-button-default uk-button-large" ><a href="#">Read More>></a></button>
+
+                                                                    </div>
+																	
+
+
+                                   </div>
+                            </div>
+							    <div class='uk-container uk-container-small  uk-width-1-1@s  uk-width-1-2@m' >
+
+
+                                  <div class="uk-card ">
+                                                                    <div class="uk-card-media-right " >
+                                                                          <img src="<?php bloginfo('template_directory') ?>/uhdpics/deliveryapps.jpg" alt="" height="700px" width="800px">
+                                                                    </div>
+                                                                 
+
+
+                                   </div>
+                            </div>
+							
+							
+							
+							
+</div>
+
+
+
+
+
+
+ <div class = 'uk-section uk-section-small uk-section-default  uk-grid '>
+ 
+ 
+     <div class='uk-container uk-container-small  uk-width-1-3@m  uk-width-1-1@s'>
+
+
+                              <div class="uk-card uk-card-default">
+                                                              <div class="uk-card-header">
+                                                                      <h3 class="uk-card-title">7-day-Trial</h3>
+                                                              </div>
+
+                                                                <div class="uk-card-media-bottom">
+                                                                    <img src="<?php bloginfo('template_directory')?>uhdpics/cloudapps.png" alt="business">
+                                                                </div>
+                                                              
+                                                                  <div class="uk-card-footer"><a href = "#">download </a><a href = "#">Android</a><a href = "#">IOS</a></div>
+
+
+                               </div>
+       </div>
+	       <div class='uk-container uk-container-small  uk-width-1-3@m  uk-width-1-1@s'>
+
+
+                              <div class="uk-card uk-card-default">
+                                                              <div class="uk-card-header">
+                                                                      <h3 class="uk-card-title">7-day-Trial</h3>
+                                                              </div>
+
+                                                                <div class="uk-card-media-bottom">
+                                                                    <img src="<?php bloginfo('template_directory')?>uhdpics/cloudapps.png" alt="business">
+                                                                </div>
+                                                              
+                                                                  <div class="uk-card-footer"><a href = "#">download</a><a href = "#">Android</a><a href = "#">IOS</a></div>
+
+
+                               </div>
+       </div>
+	       <div class='uk-container uk-container-small  uk-width-1-3@m  uk-width-1-1@s'>
+
+
+                              <div class="uk-card uk-card-default">
+                                                              <div class="uk-card-header">
+                                                                      <h3 class="uk-card-title">7-day-Trial</h3>
+                                                              </div>
+
+                                                                <div class="uk-card-media-bottom">
+                                                                    <img src="<?php bloginfo('template_directory')?>/uhdpics/cloudapps.png" alt="business">
+                                                                </div>
+                                                              
+                                                                  <div class="uk-card-footer"><a href = "#">download</a><a href = "#">Android</a><a href = "#">IOS</a></div>
+
+
+                               </div>
+       </div>
+ </div>
+
+
+
+
+<div class = 'uk-section uk-section-large  uk-padding  uk-grid ' id="ourteam">
+										 <div class='uk-container uk-container-small uk-width-1-1@s  uk-width-1-1@m'>
+																							
+																								<center><h1  uk-text-large uk-text-bolder uk-parallax="x:0,50,0"><a href="#">OUR TEAM</a></h1></center>
+																								
+										 </div>
+											<div class='uk-container uk-container-small  uk-width-1-1@s  uk-width-1-2@m' >
+
+
+																	<div class="uk-card ">
+																			<div class="uk-card-media-left " >
+																				  <img src="<?php bloginfo('template_directory')?>/uhdpics/tags.png" alt="" height="700px" width="800px">
+																			</div>
+                                                                 
+
+
+																	</div>
+											</div>
+                          <div class='uk-container uk-container-small  uk-width-1-1@s  uk-width-1-2@m'>
+
+
+                                  <div class="uk-card uk-card-default">
+                                                                   
+                                                                    <div class="uk-card-body">
+                                                                         <br><br><br><br>
+											<p uk-parallax="x:0,70,0" uk-text-large uk-text-bolder>    
+															Our Team comprises of highly qualified developers working at high level of software development for many years.<br><br>
+
+
+
+											</p>
+
+											<p uk-parallax="x:0,70,0" uk-text-large uk-text-bolder>
+												Our Team is a responsible team of professionals who values your requirements and deliver tasks  to you with free assists and Training.</i></p>
+											</p>
+											
+<br><br><br>
+																		  																	
+																						<button class="uk-button uk-button-default uk-button-large" ><a href="#">Read More>></a></button>
+
+                                  </div>
+																	
+
+
+                                   </div>
+                            </div>
+							
+							
+							
+							
+							
+</div>
